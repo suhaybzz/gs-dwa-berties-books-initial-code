0d645e95c3660e3e33e223e32b70d297b0d1e3b4
suhaybzz <suhaybahmed07@gmail.com>
2025-11-13 14:58:13 +0000

    lab 6e add books/added

--- routes/books.js
+++ routes/books.js
@@ -1,20 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET books home page. */
+// Books home page
 router.get('/', function (req, res, next) {
   res.render('books', { title: "Bertie's Books" });
 });
 
+// List all books (HTML version)
 router.get('/list', function (req, res, next) {
-  let sqlquery = 'SELECT * FROM books'; // query database to get all the books
+  let sqlquery = 'SELECT * FROM books';
 
-  // execute sql query
   db.query(sqlquery, (err, result) => {
     if (err) {
       return next(err);
     }
-    res.send(result);
+    res.render('list.ejs', { availableBooks: result });
+  });
+});
+
+// Show Add Book form
+router.get('/addbook', function (req, res, next) {
+  res.render('addbook.ejs');
+});
+
+// Handle Add Book form submission
+router.post('/bookadded', function (req, res, next) {
+  let sqlquery = 'INSERT INTO books (name, price) VALUES (?, ?)';
+  let newrecord = [req.body.name, req.body.price];
+
+  db.query(sqlquery, newrecord, (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res.send(
+      'This book is added to database, name: ' +
+        req.body.name +
+        ', price ' +
+        req.body.price
+    );
   });
 });
 
